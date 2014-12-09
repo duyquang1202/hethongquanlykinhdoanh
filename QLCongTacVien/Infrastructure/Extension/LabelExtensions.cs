3a156b91cf0f1f72e2554c12ef074eded1fc4329
Vo Duy Quang <quang.vo@audiencemedia.com>
2014-12-09 19:22:42 +0700

    fix error html

--- QLCongTacVien/Infrastructure/Extension/LabelExtensions.cs
+++ QLCongTacVien/Infrastructure/Extension/LabelExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Html;
 using System.Web.Routing;
 
 namespace QLCongTacVien.Infrastructure.Extension
